2d103d14438ab104a25e58b7efe23465ae5b90f8
Joe Menduni <jmenduni@ithaca.edu>
2018-01-31 09:49:10 -0500

    Added javadocs

--- gitPrac/src/gitPrac/practice.java
+++ gitPrac/src/gitPrac/practice.java
@@ -1,24 +1,33 @@
 package gitPrac;
 
 public class practice {
-	
 
+	/**
+	 * Chcecks to see is a string is empty
+	 * @param practice
+	 * @return
+	 */
 	public boolean checkEmpty(String practice){
-		
+
 		boolean returnString;
-		
+
 		if(practice.equals(null) || practice.equals("")){
-		    returnString = true;
+			returnString = true;
 		}else{
 			returnString = false;
 		}
 		return returnString;
 	}
-	
+
+	/**
+	 * Checks to see if a string has only one number
+	 * @param practice
+	 * @return
+	 */
 	public boolean checkOne(String practice){
-        
-		//checks if method can handle two charcaters to calculate
-        
+
+		//checks if method can handle one characters to calculate
+
 		boolean returnString=false;
 		int count =0;
 		for (int i=0; i < practice.length(); i++){
@@ -34,14 +43,20 @@ public class practice {
 				returnString = false;
 			}
 		}
-	
+
 		return returnString;
-		
+
 	}
+
+	/**
+	 * Checks to see if a string has two numbers
+	 * @param practice
+	 * @return
+	 */
 	public boolean checkTwo(String practice){
-        
-        //checks if method can handle two charcaters to calculate
-		
+
+		//checks if method can handle two charcaters to calculate
+
 		boolean returnString=false;
 		int count =0;
 		for (int i=0; i < practice.length(); i++){
@@ -57,36 +72,46 @@ public class practice {
 				returnString = false;
 			}
 		}
-	
+
 		return returnString;
-		
+
 	}
+
+	/**
+	 * Checks to see if a string has more than two numbers
+	 * @param practice
+	 * @return
+	 */
 	public boolean moreNumbers(String practice){
 		boolean returnString=false;
-		
+
 		for (int i=0; i < practice.length(); i++){
 			if(Character.isDigit(practice.charAt(i))){
-				
-				
+
+
 				returnString = true;
-				
+
 			}else{
 				returnString = false;
 			}
 		}
-	
-		return returnString;
-		
-		
-	}
-	
 
+		return returnString;
+
+
+	}
+
+
+	/**
+	 * Adds one number
+	 * @param practice
+	 */
 	public void Add(String practice){
 		int calc =0;
 		for (int i=0; i < practice.length(); i++){
 			if(Character.isDigit(practice.charAt(i))){
 				calc = calc +practice.charAt(i);
-				
+
 			}
 		}
 	}
@@ -98,7 +123,7 @@ public class practice {
 				if(count<2){
 					calc = calc +practice.charAt(i);
 				}
-				
+
 			}
 		}
 	}
@@ -110,7 +135,7 @@ public class practice {
 				if(count<3){
 					calc = calc +practice.charAt(i);
 				}
-				
+
 			}
 		}
 	}
