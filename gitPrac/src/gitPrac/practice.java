261e5dd5d10f9c81ef8d92dee0d6d87cb7762a7c
Paul Regan <pregan@ithaca.edu>
2018-01-31 06:46:08 -0500

    Third test to check whether the function can work with two numbers but no more.

--- gitPrac/src/gitPrac/practice.java
+++ gitPrac/src/gitPrac/practice.java
@@ -16,13 +16,43 @@ public class practice {
 	
 	public boolean checkOne(String practice){
 		
-		boolean returnString;
-		
-		if(practice.equals(null) || practice.equals("")){
-		    returnString = true;
-		}else{
-			returnString = false;
+		boolean returnString=false;
+		int count =0;
+		for (int i=0; i < practice.length(); i++){
+			if(practice.equals(practice.charAt(i)) || practice.equals("")){
+				count++;
+				if(count<2){
+					returnString = true;
+				}
+				else{
+					returnString = false;
+				}
+			}else{
+				returnString = false;
+			}
 		}
+	
+		return returnString;
+		
+	}
+	public boolean checkTwo(String practice){
+		
+		boolean returnString=false;
+		int count =0;
+		for (int i=0; i < practice.length(); i++){
+			if(practice.equals(practice.charAt(i)) || practice.equals("")){
+				count ++;
+				if(count<3){
+					returnString = true;
+				}
+				else{
+					returnString = false;
+				}
+			}else{
+				returnString = false;
+			}
+		}
+	
 		return returnString;
 		
 	}
