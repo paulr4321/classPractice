592672888e0a5e0dfeccc33ac58ba604069d1576
Paul Regan <pregan@ithaca.edu>
2018-02-01 10:34:18 -0500

    Adding implementation changes for add function

--- gitPrac/src/gitPrac/practice.java
+++ gitPrac/src/gitPrac/practice.java
@@ -2,116 +2,28 @@ package gitPrac;
 
 public class practice {
 	
+	public void practice(){
+		
+	}
 
-	public boolean checkEmpty(String practice){
-		
-		boolean returnString;
-		
-		if(practice.equals(null) || practice.equals("")){
-		    returnString = true;
-		}else{
-			returnString = false;
-		}
-		return returnString;
-	}
-	
-	public boolean checkOne(String practice){
-        
-		//checks if method can handle two charcaters to calculate
-        
-		boolean returnString=false;
-		int count =0;
-		for (int i=0; i < practice.length(); i++){
-			if(Character.isDigit(practice.charAt(i)) || practice.equals("")){
-				count++;
-				if(count<2){
-					returnString = true;
-				}
-				else{
-					returnString = false;
-				}
-			}else{
-				returnString = false;
-			}
-		}
-	
-		return returnString;
-		
-	}
-	public boolean checkTwo(String practice){
-        
-        //checks if method can handle two charcaters to calculate
-		
-		boolean returnString=false;
-		int count =0;
-		for (int i=0; i < practice.length(); i++){
-			if(Character.isDigit(practice.charAt(i)) || practice.equals("")){
-				count ++;
-				if(count<3){
-					returnString = true;
-				}
-				else{
-					returnString = false;
-				}
-			}else{
-				returnString = false;
-			}
-		}
-	
-		return returnString;
-		
-	}
-	public boolean moreNumbers(String practice){
-		boolean returnString=false;
-		
-		for (int i=0; i < practice.length(); i++){
-			if(Character.isDigit(practice.charAt(i))){
-				
-				
-				returnString = true;
-				
-			}else{
-				returnString = false;
-			}
-		}
-	
-		return returnString;
-		
-		
-	}
 	
 
-	public void Add(String practice){
+	public int Add(String practice){
 		int calc =0;
-		for (int i=0; i < practice.length(); i++){
-			if(Character.isDigit(practice.charAt(i))){
-				calc = calc +practice.charAt(i);
+		String[] listPrac = practice.split(",");
+		if(listPrac.length >= 1 && listPrac[0]!=""){
+			for (int i=0; i < listPrac.length; i++){
+				calc = calc +Integer.parseInt(listPrac[i]);
 				
 			}
 		}
-	}
-	public void StringCalculatorOneNum(String practice){
-		int calc =0;
-		int count =0;
-		for (int i=0; i < practice.length(); i++){
-			if(Character.isDigit(practice.charAt(i))){
-				if(count<2){
-					calc = calc +practice.charAt(i);
-				}
-				
-			}
+		else{
+			return 0;
 		}
+		
+		return calc;
 	}
-	public void StringCalculatorTwoNum(String practice){
-		int calc =0;
-		int count =0;
-		for (int i=0; i < practice.length(); i++){
-			if(Character.isDigit(practice.charAt(i))){
-				if(count<3){
-					calc = calc +practice.charAt(i);
-				}
-				
-			}
-		}
-	}
+
+
+	
 }
